8583eb485b11d75490b5175235d61e4eaab3643c
Daan Koolman <>
2020-03-01 21:32:08 +0100

    (finalised about me page)=> created all content and pictures for about me page

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import { withNamespaces } from 'react-i18next';
-import {Helmet} from "react-helmet";
-
 
 class About extends Component {
 
@@ -9,46 +7,39 @@ class About extends Component {
 
    	const { t } = this.props;
 
-
     return (
 		<div>
 
-		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"/>
-
-        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"/>
-
-        <h1 className="h1">Image Gallery Transition</h1>
-
-
         <div id="aboutPage">
-            <div class="item red">
+            <div class="item aboutme">
               <img src="https://d2rdhxfof4qmbb.cloudfront.net/wp-content/uploads/20190227192745/Skier.jpg"/>
-              <i class="fa fa-camera"></i>
+              <i class="fa fa-forward"><p>{t('about_ski')}</p></i>
             </div>
 
-            <div class="item gray">
+            <div class="item aboutme">
               <img src="https://thegreyalley.com/wp-content/uploads/2018/06/Football-Turfs-Banner.jpg"/>
-              <i class="fa fa-paw"></i>
+              <i class="fa fa-users"><p>{t('about_football')}</p></i>
             </div>
 
-            <div class="item teal">
+            <div class="item aboutme">
               <img src="https://cdn.images.express.co.uk/img/dynamic/59/590x/watch-tour-de-france-691467.jpg"/>
-              <i class="fa fa-cloud"></i>
+              <i class="fa fa-bicycle"><p>{t('about_cycling')}</p></i>
+
             </div>
 
-            <div class="item red">
-              <img src="https://hetwapenvanwesepe.nl/wp-content/uploads/2018/03/pubquiz-profielfoto-bresactiviteiten.nl_.jpg"/>
-              <i class="fa fa-camera"></i>
+            <div class="item aboutme">
+              <img src="https://www.gasterijsmits.nl/wp-content/uploads/activiteiten-gasterij-smits-pubquiz.jpg"/>
+              <i class="fa fa-search"><p>{t('about_pubquiz')}</p></i>
             </div>
 
-            <div class="item gray">
+            <div class="item aboutme">
               <img src="https://redgrapevine.com.au/wp-content/uploads/sites/15/2018/11/australian-cuisines.png"/>
-              <i class="fa fa-paw"></i>
+              <i class="fa fa-beer"><p>{t('about_drinks')}</p></i>
             </div>
 
-            <div class="item teal">
+            <div class="item aboutme">
               <img src="https://www.wearencc.com/wp-content/uploads/2018/10/MindGames-518x294.jpg"/>
-              <i class="fa fa-cloud"></i>
+              <i class="fa fa-magic"><p>{t('about_mindgames')}</p></i>
             </div>
         </div>
 
