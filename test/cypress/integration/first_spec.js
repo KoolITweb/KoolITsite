fb6f1502df16390e3f8812cd7a3a350d10f72035
KoolITweb <daan_koolman@hotmail.com>
2020-08-22 11:54:34 +0200

    Added first set up of tests

--- test/cypress/integration/first_spec.js
+++ test/cypress/integration/first_spec.js
@@ -1,5 +1,47 @@
+//make url a variable to be able to test against localhost and against live version
+
+
 describe('My First Test', function() {
-  it('Does not do much!', function() {
-    expect(true).to.equal(false)
+  it('Visits the Kitchen Sink', () => {
+    cy.visit('https://koolit.nl')
+
+    //click on menu option based on text
+    cy.contains('Resume').click()
+
+    // Should be on a new URL which includes '/commands/actions'
+    cy.url().should('include', '/resume')
+
+    // Scroll down to get TNT logo into view
+
+    // Go to projects via projects button on TNT page
+    cy.get('.mdl-button__ripple-container')[5]
+      .click()
+
+    cy.url().should('include', '/projects')
+
+    //check amount of projects is at least 6
+    cy.get('.projects-grid').find('.Card').its('length').should('be.gte', 6)
+
+    //Or to be able to do something with the amount of projects found
+    cy.get('.projects-grid')
+      .find('.Card')
+      .then(projects => {
+        const projectsCount = Cypress.$(projects).length;
+        expect(projectsCount).to.be.at.least(6);
+      });
+
   })
 })
+
+
+//Test resizing page to check change in element, header change
+
+//Check ppc slidedeck can be slided via button, dot and picture
+
+//Check ppc slidedeck can be played automatically
+
+//Check hover on about me images provides information/text
+
+//Check contact page contains right email and contains the floating cabbage
+
+//Open and close each project on the projects page
--- test/webdriverIO/specs/first_test.js
+++ test/webdriverIO/specs/first_test.js
@@ -1,5 +1,8 @@
 const assert = require('assert')
 
+//make url a variable to be able to test against localhost and against live version
+
+
 describe('webdriver.io page', () => {
     it('should have the right title', async () => {
         await browser.url('https://webdriver.io')
@@ -8,3 +11,17 @@ describe('webdriver.io page', () => {
         return expect(title).to.equal('WebdriverIP · Next-gen WebDriver test framework for Node.js')
     })
 })
+
+
+
+//Test resizing page to check change in element, header change
+
+//Check ppc slidedeck can be slided via button, dot and picture
+
+//Check ppc slidedeck can be played automatically
+
+//Check hover on about me images provides information/text
+
+//Check contact page contains right email and contains the floating cabbage
+
+//Open and close each project on the projects page
